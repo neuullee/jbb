1ddfb6421744d84f268c398e65ddb4393c2f30d3
neuullee <neuull.ee@gmail.com>
2025-11-27 10:47:29 +0900

    객체 생성시 id 증가 --> 연습하기

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
+// 객체가 생성시 마다 번호 증가
 public class Main {
     public static void main(String[] args) {
-        System.out.println("안녕");
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
+        System.out.println(new Article().id);
     }
+}
+
+class Article {
+    static int lastId;   //static 초기화 블럭에서 사용됨
+    int id;
+
+    static {             //static 초기화 블럭, 프로그램 시작시 1회 / 메모리 상단에 위치함(제일먼저 실행됨)
+        lastId = 0;
+    }
+
+    Article() {             // default 생성자
+        this(lastId + 1);   // this() --> 다른 생성자 호출
+        lastId++;
+    }
+
+    Article(int id) {       //생성자 오버로딩
+        this.id = id;
+    }
+
 }
